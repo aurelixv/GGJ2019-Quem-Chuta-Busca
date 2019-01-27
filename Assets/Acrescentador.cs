82e2b1e1dadfdd76a950743efe4bea8da6facec9
Aurélio Vinícius <aurelius@Aurelius-MBP.local>
2019-01-27 02:27:39 -0200

    asdasd

---
+++ Assets/Acrescentador.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Acrescentador : MonoBehaviour
+{
+
+    public Slider slider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider.value = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        slider.value = Time.time * 0.05f;
+    }
+}
