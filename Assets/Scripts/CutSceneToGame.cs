8c99849b2022202588d978b07946d4c1891c2fec
wellingtondev <wlss.lucas@gmail.com>
2019-01-27 09:09:31 -0200

    old man

--- Assets/Scripts/CutSceneToGame.cs
+++ Assets/Scripts/CutSceneToGame.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneToGame : MonoBehaviour {
     // Start is called before the first frame update
