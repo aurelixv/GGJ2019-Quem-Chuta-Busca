669d8acc46b2d5ce34b0092865d7aa20ad4469da
Lincoln Batista <linkskill@gmail.com>
2019-01-27 09:57:01 -0200

    Teste

--- Assets/Scripts/CutSceneToGame.cs
+++ Assets/Scripts/CutSceneToGame.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class SceneToGame : MonoBehaviour {
+using UnityEngine.SceneManagement;
+public class CutSceneToGame : MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
+
+        while (AnimationState.time >= AnimationState.length)
+            yield return null;
+        
         SceneManager.LoadScene("main 1");
     }
 }
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour {
 
     public void PlayGame() {
+        Debug.Log("Entrou!!!!");
         SceneManager.LoadScene("CutScene");
     }
 
