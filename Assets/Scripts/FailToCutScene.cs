1c983701722aef29ac784e0d241a2df089e3cbbd
Lincoln Batista <linkskill@gmail.com>
2019-01-27 23:07:58 -0200

    Integrando as cenas

---
+++ Assets/Scripts/FailToCutScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FailToCutScene : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+        int sec = 2;
+        StartCoroutine(Wait(sec));
+    }
+
+    IEnumerator Wait(int sec)
+    {
+        yield return new WaitForSeconds(sec);
+        SceneManager.LoadScene("CutScene");
+    }
+}
