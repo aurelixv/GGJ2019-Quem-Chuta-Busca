ed43f0aaaea62f10e73a5593a149fe7c6cdd071d
Lincoln Batista <linkskill@gmail.com>
2019-01-27 23:12:08 -0200

    Arrumando bugs nos Scripts

--- Assets/Scripts/CutSceneToGame.cs
+++ Assets/Scripts/CutSceneToGame.cs
@@ -6,6 +6,13 @@ using UnityEngine.SceneManagement;
 public class SceneToGame : MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
+        int sec = 2;
+        StartCoroutine(Wait(sec));
+    }
+    
+    IEnumerator Wait(int sec)
+    {
+        yield return new WaitForSeconds(sec);
         SceneManager.LoadScene("main 1");
     }
 }
