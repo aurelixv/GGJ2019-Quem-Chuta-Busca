205a1dbea03b9e409f2068e8632942761851277b
Lincoln Batista <linkskill@gmail.com>
2019-01-28 11:16:50 -0200

    fix

--- Assets/Scripts/CutSceneToGame.cs
+++ Assets/Scripts/CutSceneToGame.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneToGame : MonoBehaviour {
+public class CutSceneToGame : MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
         int sec = 2;
         StartCoroutine(Wait(sec));
     }
-    
+
     IEnumerator Wait(int sec)
     {
         yield return new WaitForSeconds(sec);
